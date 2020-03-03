6abdc5eed73dda291d9c0cc376fb882bf1fd8570
Albert Suarez <alsumo95@gmail.com>
2020-03-03 19:50:37 +0100

    Started implementing cron process

--- src/config.py
+++ src/config.py
@@ -11,6 +11,26 @@ DATA_TIMEOUT = 30
 DATA_RTD = 15
 USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; FSL 7.0.6.01001)'
 URL_BASE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data'
-URL_CONFIRMED = f'{URL_BASE}/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
-URL_DEATHS = f'{URL_BASE}/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
-URL_RECOVERED = f'{URL_BASE}/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
+
+# Time
+TIME_BETWEEN_RESOURCES = 15
+
+# Resources
+DATA_FOLDER = 'data'
+RESOURCES = [
+    {
+        'name': 'Confirmed',
+        'data_url': f'{URL_BASE}/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
+        'data_path': f'{DATA_FOLDER}/confirmed.json'
+    },
+    {
+        'name': 'Deaths',
+        'data_url': f'{URL_BASE}/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
+        'data_path': f'{DATA_FOLDER}/deaths.json'
+    },
+    {
+        'name': 'Recovered',
+        'data_url': f'{URL_BASE}/csse_covid_19_time_series/time_series_19-covid-Recovered.csv',
+        'data_path': f'{DATA_FOLDER}/recovered.json'
+    }
+]
--- src/cron/runner.py
+++ src/cron/runner.py
@@ -1,6 +1,24 @@
+import json
+import os
+import time
+
+from src.config import RESOURCES, TIME_BETWEEN_RESOURCES
+from src.helper import log
+from src.johns_hopkins import retriever
+
+
 def run():
     """
     Main function in order to process a new iteration of the Twitter bot.
     :return: Iteration done.
     """
-    pass
+    for item in RESOURCES:
+        item_name = item['name']
+        log.info(f'Processing {item_name}...')
+        results = retriever.get_last_update(item['data_url'])
+        if results is not None:
+            item_data_path = item['data_path']
+            item_data_path_exists = os.path.isfile(item_data_path)
+            with open(item_data_path, 'w') as item_data_file:
+                json.dump(results, item_data_file)
+        time.sleep(TIME_BETWEEN_RESOURCES)
