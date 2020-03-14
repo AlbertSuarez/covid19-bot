7db341b56593946c6e1b43cda339629769bfced8
Albert Suarez <alsumo95@gmail.com>
2020-03-14 21:24:54 +0100

    Inspected code

--- src/johns_hopkins/retriever.py
+++ src/johns_hopkins/retriever.py
@@ -4,7 +4,8 @@ import requests
 
 from io import StringIO
 
-from src.config import JOHNS_HOPKINS_USER_AGENT, JOHNS_HOPKINS_DATA_ATTEMPTS, JOHNS_HOPKINS_DATA_TIMEOUT, JOHNS_HOPKINS_DATA_RTD
+from src.config import JOHNS_HOPKINS_USER_AGENT, JOHNS_HOPKINS_DATA_ATTEMPTS, JOHNS_HOPKINS_DATA_TIMEOUT, \
+    JOHNS_HOPKINS_DATA_RTD
 from src.helper import log
 
 
@@ -17,7 +18,9 @@ def get_last_update(data_url):
     result_dict = dict()
     for attempt in range(JOHNS_HOPKINS_DATA_ATTEMPTS):
         try:
-            response = requests.get(data_url, headers={'User-Agent': JOHNS_HOPKINS_USER_AGENT}, timeout=JOHNS_HOPKINS_DATA_TIMEOUT)
+            response = requests.get(
+                data_url, headers={'User-Agent': JOHNS_HOPKINS_USER_AGENT}, timeout=JOHNS_HOPKINS_DATA_TIMEOUT
+            )
             rows = [row for row in csv.reader(StringIO(response.text), delimiter=',')][1:]
             for row in rows:
                 dict_key = row[1] if not row[0] else f'{row[0]}, {row[1]}'
